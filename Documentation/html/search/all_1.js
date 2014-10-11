4a4cf499a8b4371533c218aad8c998e72da1625b
JoeyAndres <jandres@ualberta.ca>
2014-10-11 13:30:07 -0600

    Policy documented

--- Documentation/html/search/all_1.js
+++ Documentation/html/search/all_1.js
@@ -4,8 +4,9 @@ var searchData=
   ['actuator_3c_20a_20_3e',['Actuator&lt; A &gt;',['../classAI_1_1Actuator.html',1,'AI']]],
   ['addaction',['addAction',['../classAI_1_1StateActionPairContainer.html#af4c9faef1c7d4e35b11f95b8a84352ae',1,'AI::StateActionPairContainer']]],
   ['addstate',['addState',['../classAI_1_1StateActionPairContainer.html#a12842518174d0af5fc89c7a86e766099',1,'AI::StateActionPairContainer']]],
-  ['agent',['Agent',['../classAI_1_1Agent.html#a2cab0b2534dc40185a3e2f9a858e8023',1,'AI::Agent']]],
   ['agent',['Agent',['../classAI_1_1Agent.html',1,'AI']]],
+  ['agent',['Agent',['../classAI_1_1Agent.html#a2cab0b2534dc40185a3e2f9a858e8023',1,'AI::Agent']]],
   ['ai',['AI',['../namespaceAI.html',1,'']]],
-  ['argmax',['argMax',['../classAI_1_1Algorithm_1_1Policy_1_1EpsilonGreedy.html#a1a7489821592b81576e5bc8674fc43c3',1,'AI::Algorithm::Policy::EpsilonGreedy::argMax()'],['../classAI_1_1Algorithm_1_1ReinforcementLearning.html#ad1d8a8ebb47fb71a53b15b770795e286',1,'AI::Algorithm::ReinforcementLearning::argMax()']]]
+  ['argmax',['argMax',['../classAI_1_1Algorithm_1_1Policy_1_1EpsilonGreedy.html#a1a7489821592b81576e5bc8674fc43c3',1,'AI::Algorithm::Policy::EpsilonGreedy::argMax()'],['../classAI_1_1Algorithm_1_1ReinforcementLearning.html#ad1d8a8ebb47fb71a53b15b770795e286',1,'AI::Algorithm::ReinforcementLearning::argMax()']]],
+  ['ai_20project_20documentation',['AI Project Documentation',['../index.html',1,'']]]
 ];
---
+++ Documentation/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ai_20project_20documentation',['AI Project Documentation',['../index.html',1,'']]]
+];
